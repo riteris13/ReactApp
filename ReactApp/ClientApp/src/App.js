318c3a543ace960d17e5b9d83f986e06d07e01ee
Vladimiras Desiatnikovas <riteris13@gmail.com>
2019-11-18 12:55:44 +0000

    net core 2

--- ReactApp/ClientApp/src/App.js
+++ ReactApp/ClientApp/src/App.js
@@ -5,17 +5,15 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
-import './custom.css'
-
 export default class App extends Component {
-  static displayName = App.name;
+  displayName = App.name
 
-  render () {
+  render() {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/fetchdata' component={FetchData} />
       </Layout>
     );
   }
